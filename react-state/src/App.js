78526934a852b2457c2a6bd327b32861ad12296c
Rushali Sarkar <rushalisarkar2000@gmail.com>
2021-08-01 23:23:52 +0530

    Added app

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function SecretComponent() {
   return (
     <h1>This is a piece of information for authorised users only</h1>
@@ -14,7 +15,6 @@ function RegularComponent() {
 }
 
 const [first, second, third, fourth] = ["React", "Machine Learning", "Game Development", "App Development"];
-
 
 function App({authorised}) {
   const [emotion, setEmotion] = useState("Happy");
@@ -38,17 +38,21 @@ function App({authorised}) {
       </ul>
       <button onClick={() => setCounter(counter + 1)}>{counter}</button>
       <h1>My current Emotion is {emotion} and {secondaryEmotion}.</h1>
+      <div>
       <button onClick={() => setEmotion("Happy")}>Make Happy</button>
       <button onClick={() => setEmotion("Sad")}>Make Sad</button>
       <button onClick={() => setEmotion("Excited")}>Make Excited</button>
       <button onClick={() => setEmotion("Frustrated")}>Make Frustrated</button>
       <button onClick={() => setEmotion("Amazed")}>Make Amazed</button>
+      </div>
       <br/>
+      <div>
       <button onClick={() => setSecondaryEmotion("Tired")}>Make Tired</button>
       <button onClick={() => setSecondaryEmotion("Exhausted")}>Make Exhausted</button>
       <button onClick={() => setSecondaryEmotion("Confused")}>Make Confused</button>
       <button onClick={() => setSecondaryEmotion("Clueless")}>Make Clueless</button>
       <button onClick={() => setSecondaryEmotion("Energised")}>Make Energised</button>
+      </div>
     </React.Fragment>
   );
 }
